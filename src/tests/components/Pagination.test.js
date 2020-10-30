8c1fc20dbf6db86ac02ea46f2cbc71dc9132efa6
Tomasz Górecki <tomasz.gorecki@dreamlab.pl>
2020-10-30 23:18:18 +0100

    - tests for pagination - updated

--- src/tests/components/Pagination.test.js
+++ src/tests/components/Pagination.test.js
@@ -3,11 +3,11 @@ import { shallow } from "enzyme";
 import { Pagination } from "../../components/shared/Pagination/Pagination";
 
 describe('<Pagination/>', () => {
+    const mockFunction = jest.fn();
     let wrapper;
 
     beforeEach(() => {
-        wrapper = shallow(<Pagination handlePageChange={() => {
-        }}/>);
+        wrapper = shallow(<Pagination handlePageChange={mockFunction}/>);
     });
 
     it('should rendered two buttons', () => {
@@ -17,5 +17,25 @@ describe('<Pagination/>', () => {
             expect(node.hasClass('btn')).toEqual(true);
             expect(node.text().includes('page')).toEqual(true)
         });
+    });
+
+    it('should render button for previous page', () => {
+        wrapper.setProps({ hasNextPage: true, hasPrevPage: true });
+        const button = wrapper.find('button').at(0);
+        expect(button.text()).toEqual('Previous page');
+        expect(button.hasClass('btn btn-dark')).toEqual(true);
+        button.simulate('click');
+        expect(mockFunction.mock.calls).toHaveLength(1);
+        expect(mockFunction.mock.calls[0][0]).toEqual("PREVIOUS_PAGE");
+    });
+
+    it('should render button for next page', () => {
+        wrapper.setProps({ hasNextPage: true, hasPrevPage: true });
+        const button = wrapper.find('button').at(1);
+        expect(button.text()).toEqual('Next page');
+        expect(button.hasClass('btn btn-light')).toEqual(true);
+        button.simulate('click');
+        expect(mockFunction.mock.calls).toHaveLength(1)
+        expect(mockFunction.mock.calls[0][0]).toEqual("NEXT_PAGE");
     });
 });
